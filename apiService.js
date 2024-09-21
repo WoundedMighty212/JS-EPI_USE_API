16b5e14597dbcb846c2a65762c5804ea9f34cfc8
WoundedMighty212 <michael.ludick13@outlook.com>
2024-09-21 12:13:57 +0200

    added API services and calls to app

--- apiService.js
+++ apiService.js
@@ -16,3 +16,19 @@ export async function CheckServerAvailabilty() {
        // throw error;
     }
 }
+export async function GetApiData(querryString){
+    try{
+        const response = await axios.get(`${API_BASE_URL}${querryString}`, 
+            { 
+                headers: 
+                { 
+                    'candidate-token': `${assignedToken}`,
+                } 
+            });
+            return response.data; 
+    }
+    catch(error){
+        console.error
+        ('Error Reaching Server: Server not found', error);
+    }
+}
--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 import {CheckServerAvailabilty} from './apiService.js'
+import {GetApiData} from './apiService.js'
 
 function RunApp(){
     try{
+        const getEmployeeQuery = 
+        `api/employee-sorter/get-employees?limit=500&skip=0`;
+        const getReporteeQuery = 
+        `api/employee-sorter/get-reporting-relationship?limit=500&skip=0`;
         const serverStatus =  CheckServerAvailabilty()
-        .then(serverStatus => 
-            console.log('Server Status:', serverStatus));
+        .then(Status => console.log('Server Status:', Status));
+
+        const employee = GetApiData(getEmployeeQuery)
+        .then(employee => console.log('employee data:', employee));
+
+        const ReporteeRelationship =
+        GetApiData(getReporteeQuery)
+        .then(Reportee => console.log('Reportee data:', Reportee));
     }
     catch(error){
         console.error('Error fetching server status:', error);
