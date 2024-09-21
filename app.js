adc5001c7714d9ed828d6859b62fbb14a6f03bcd
WoundedMighty212 <michael.ludick13@outlook.com>
2024-09-21 12:28:15 +0200

    server status checker and call api change

--- app.js
+++ app.js
@@ -10,14 +10,21 @@ function RunApp(){
         `api/employee-sorter/get-reporting-relationship?limit=500&skip=0`;
 
         const serverStatus =  CheckServerAvailabilty()
-        .then(Status => console.log('Server Status:', Status));
+        .then(Status => {
+            if(Status['message'] === 'Service is running'){
+                console.log('Server is online')
 
-        const employee = GetApiData(getEmployeeQuery)
-        .then(employee => console.log('employee data:', employee));
+                const employee = GetApiData(getEmployeeQuery)
+                .then(employee => console.log('employee data:', employee));
 
-        const ReporteeRelationship =
-        GetApiData(getReporteeQuery)
-        .then(Reportee => console.log('Reportee data:', Reportee));
+                const ReporteeRelationship =
+                GetApiData(getReporteeQuery)
+                .then(Reportee => console.log('Reportee data:', Reportee));
+
+            }else{
+                console.log('Server is not running or unavailable')
+            }
+        });  
     }
     catch(error){
         console.error('App-Run Error', error);
